93de7557fe706d933fae191c4bd9ff72d8a84fbb
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-29 01:17:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const output = document.getElementById("output");
+
+// Create an array of five promises, each of which resolves with a random time between 1 and 5 seconds. Use Promise.any to wait for the first promise to resolve, and then print its result to the div of id output.
+async function createPromise(time){
+    let promise = new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve(time);
+        }, time)
+    })
+    return promise;
+}
+
+let promises = Promise.any([
+    createPromise(1000), 
+    createPromise(3000),
+    createPromise(2000),
+    createPromise(5000),
+    createPromise(4000),
+]);
+//  prom.then((data)=>{
+//     output.innerHTML = data;
+
+// });
+
+
